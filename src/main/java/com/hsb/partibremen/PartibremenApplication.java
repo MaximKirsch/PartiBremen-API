a9535919bf5e89cedcc1a46f0a0df64b0e1f180e
Maxim <m.kirsch@28apps.de>
2024-04-05 13:08:49 +0200

    Initial commit

---
+++ src/main/java/com/hsb/partibremen/PartibremenApplication.java
@@ -0,0 +1,13 @@
+package com.hsb.partibremen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartibremenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartibremenApplication.class, args);
+	}
+
+}
