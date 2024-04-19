e68a372f8c8f63d1b564680e203330619f827191
Maxim <m.kirsch@28apps.de>
2024-04-19 13:07:58 +0200

    add db requests to controller

--- src/main/java/com/hsb/partibremen/entities/repo/UserRepo.java
+++ src/main/java/com/hsb/partibremen/entities/repo/UserRepo.java
@@ -1,14 +1,12 @@
 package com.hsb.partibremen.entities.repo;
 
+import com.hsb.partibremen.entities.model.user.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface userRepo {
+import java.util.UUID;
 
-    @Transactional
-    @Modifying
-    @Query
-    public void vfindOneUser(String userId);
+public interface UserRepo extends BaseRepo<User, UUID> {
 
 }
