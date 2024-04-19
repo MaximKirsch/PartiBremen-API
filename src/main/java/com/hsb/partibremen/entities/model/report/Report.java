58afc055abd28edfd0248c82c6253942776bf864
ojou-less <mark.fischer0404@gmail.com>
2024-04-19 12:49:00 +0200

    report jetzt in der DB

--- src/main/java/com/hsb/partibremen/entities/model/report/Report.java
+++ src/main/java/com/hsb/partibremen/entities/model/report/Report.java
@@ -2,10 +2,17 @@ package com.hsb.partibremen.entities.model.report;
 
 import com.hsb.partibremen.entities.util.BaseEntity;
 import com.hsb.partibremen.entities.util.BaseService;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "report")
 public class Report extends BaseEntity {
 
+    @Column
     private String kommentar;
+    @Column
     private String title; //.
 
     public String getKommentar() {
