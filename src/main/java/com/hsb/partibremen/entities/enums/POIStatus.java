26bedacb1456e89262c9f9e0b4b28305bb12461b
ojou-less <mark.fischer0404@gmail.com>
2024-04-23 13:48:29 +0200

    enums idee

--- src/main/java/com/hsb/partibremen/entities/enums/POIStatus.java
+++ src/main/java/com/hsb/partibremen/entities/enums/POIStatus.java
@@ -1,5 +1,35 @@
 package com.hsb.partibremen.entities.enums;
 
 public enum POIStatus {
-    UNPLANNED, PLANNED, IN_PROGRESS, FINISHED, DISCARDED, CANCELLED, DELETED;
+    UNPLANNED("Nicht geplant", 0),
+    PLANNED("Geplant", 1),
+    IN_PROGRESS("In Arbeit", 2),
+    FINISHED("Fertig", 3),
+    DISCARDED("Verworfen", 4),
+    CANCELLED("Abgebrochen", 5),
+    DELETED("Gelöscht", 6);
+
+    private String bezeichnung;
+    private int id;
+
+    POIStatus(String bezeichnung, int id){
+        this. bezeichnung = bezeichnung;
+        this.id = id;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/hsb/partibremen/entities/enums/Role.java
+++ src/main/java/com/hsb/partibremen/entities/enums/Role.java
@@ -1,5 +1,34 @@
 package com.hsb.partibremen.entities.enums;
 
 public enum Role {
-    DEFAULT, USER, CREATOR, MODERATOR, ADMIN, DECIDER;
+    DEFAULT("DEFAULT", 0),
+    USER("User", 1),
+    CREATOR("Creator", 2),
+    MODERATOR("Moderator", 3),
+    ADMIN("Admin", 4),
+    DECIDER("Decider", 5);
+
+    private String bezeichung;
+    private int id;
+
+    Role(String bezeichnung, int id){
+        this.bezeichung = bezeichnung;
+        this.id = id;
+    }
+
+    public String getBezeichung() {
+        return bezeichung;
+    }
+
+    public void setBezeichung(String bezeichung) {
+        this.bezeichung = bezeichung;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
