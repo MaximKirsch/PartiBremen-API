ad4c4667acc098e8f27b93052b69f44eef3ed950
MaximKirsch <Maximkirsch2003@hotmail.com>
2024-04-24 16:20:42 +0200

    prepare signature

---
+++ src/main/java/com/hsb/partibremen/entities/model/petition/signature/signature.java
@@ -0,0 +1,9 @@
+package com.hsb.partibremen.entities.model.petition.signature;
+
+public class signature {
+
+    // private Petition petition...
+
+    // private User signer...
+
+}
---
+++ src/main/java/com/hsb/partibremen/entities/model/petition/signature/signatureDto.java
@@ -0,0 +1,9 @@
+package com.hsb.partibremen.entities.model.petition.signature;
+
+public class signatureDto {
+
+    //private signerId..
+
+    //private petitionId..
+
+}
