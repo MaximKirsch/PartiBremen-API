1e300daa871b7ffd7b9ced00df4ff1bb40e75cd5
SebiTV <sebastian.skrodzki@stevekist.de>
2024-05-04 10:55:25 +0200

    Update Funktion Poi is now possible

--- src/main/java/com/hsb/partibremen/entities/service/PoIService.java
+++ src/main/java/com/hsb/partibremen/entities/service/PoIService.java
@@ -2,6 +2,7 @@ package com.hsb.partibremen.entities.service;
 
 import com.hsb.partibremen.entities.model.poi.PoI;
 import com.hsb.partibremen.entities.model.poi.PoIDto;
+import com.hsb.partibremen.entities.model.user.User;
 import com.hsb.partibremen.entities.repo.PoIRepo;
 import com.hsb.partibremen.entities.util.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,24 @@ public class PoIService extends BaseService {
         }
         poi.setCreator(userService.findOne(poiDto.getCreatorId()).get());
         return poiRepo.save(poi);
+    }
+
+    public Optional<PoI> update(String id, PoIDto poiDto) {
+        Optional<PoI> optionalPoI = poiRepo.findById(UUID.fromString(id));
+        if (optionalPoI.isPresent()) {
+            PoI poi = optionalPoI.get();
+            poi.setTitel(poiDto.getTitel());
+            poi.setDescription(poiDto.getDescription());
+            poi.setActive(poiDto.getActive());
+            poi.setLatitude(poiDto.getLatitude());
+            poi.setLongitude(poiDto.getLongitude());
+            if(!(userService.findOne(poiDto.getCreatorId())).isPresent()){
+                throw new RuntimeException();
+            }
+            poi.setCreator(userService.findOne(poiDto.getCreatorId()).get());
+            poiRepo.save(poi);
+        }
+        return optionalPoI;
     }
 
     public List<PoI> findAll() {
