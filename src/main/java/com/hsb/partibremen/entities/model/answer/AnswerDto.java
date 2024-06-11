89c62133025c6dc22418a2edea5a99ba09d0c48c
OmarZiyada <omar70@schule.bremen.de>
2024-06-11 23:28:49 +0200

    Refactored AnswerDto and AnswerService to improve validation and simplify data handling
    - Removed unnecessary User attribute from AnswerDto; replaced with answererId.
    - Added validation in AnswerService to ensure exactly one type of answer (multiple choice, skalar, or text) is provided in the request.
    - Updated Answer creation logic to handle the new validation rules and ensure data consistency.

--- src/main/java/com/hsb/partibremen/entities/model/answer/AnswerDto.java
+++ src/main/java/com/hsb/partibremen/entities/model/answer/AnswerDto.java
@@ -5,62 +5,61 @@ import java.util.UUID;
 import com.hsb.partibremen.entities.enums.MultipleChoice;
 import com.hsb.partibremen.entities.model.user.User;
 
-
-
 public class AnswerDto {
-
 
     private String titel;
     private MultipleChoice multipleChoiceAnswer;
     private int skalarAnswer;
     private String textAnswer;
     private UUID questionId;
-    private UUID userId;
-    private User answerer;
+    private UUID answererId;
 
+    public UUID getAnswererId() {
+        return answererId;
+    }
 
-    
-    public User getAnswerer() {
-        return answerer;
+    public void setAnswererId(UUID answererId) {
+        this.answererId = answererId;
     }
-    public void setAnswerer(User answerer) {
-        this.answerer = answerer;
-    }
+
     public String getTitel() {
         return this.titel;
     }
+
     public void setTitel(String titel) {
         this.titel = titel;
     }
-    public UUID getUserId() {
-        return userId;
-    }
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
+
     public UUID getQuestionId() {
         return this.questionId;
     }
+
     public void setQuestionId(UUID questionId) {
         this.questionId = questionId;
     }
+
     public MultipleChoice getMultipleChoiceAnswer() {
         return multipleChoiceAnswer;
     }
+
     public void setMultipleChoiceAnswer(MultipleChoice multipleChoiceAnswer) {
         this.multipleChoiceAnswer = multipleChoiceAnswer;
     }
+
     public int getSkalarAnswer() {
         return skalarAnswer;
     }
+
     public void setSkalarAnswer(int skalarAnswer) {
         this.skalarAnswer = skalarAnswer;
     }
+
     public String getTextAnswer() {
         return textAnswer;
     }
+
     public void setTextAnswer(String textAnswer) {
         this.textAnswer = textAnswer;
     }
-    
+
 }
--- src/main/java/com/hsb/partibremen/entities/service/AnswerService.java
+++ src/main/java/com/hsb/partibremen/entities/service/AnswerService.java
@@ -34,19 +34,34 @@ public class AnswerService extends BaseService {
     public Answer create(AnswerDto answerDto) throws UserNotFoundException, QuestionNotFoundException {
         Answer answer = new Answer();
         answer.setTitel(answerDto.getTitel());
-        answer.setMultipleChoiceAnswer(answerDto.getMultipleChoiceAnswer());
-        answer.setSkalarAnswer(answerDto.getSkalarAnswer());
-        answer.setTextAnswer(answerDto.getTextAnswer());
+
+        // Validate that exactly one of the answer fields is set
+        boolean isMultipleChoiceSet = answerDto.getMultipleChoiceAnswer() != null;
+        boolean isSkalarSet = answerDto.getSkalarAnswer() != 0;
+        boolean isTextSet = answerDto.getTextAnswer() != null;
+
+        int countSetAnswers = (isMultipleChoiceSet ? 1 : 0) + (isSkalarSet ? 1 : 0) + (isTextSet ? 1 : 0);
+        if (countSetAnswers != 1) {
+            throw new IllegalArgumentException("Exactly one type of answer must be provided");
+        }
+
+        if (isMultipleChoiceSet) {
+            answer.setMultipleChoiceAnswer(answerDto.getMultipleChoiceAnswer());
+        } else if (isSkalarSet) {
+            answer.setSkalarAnswer(answerDto.getSkalarAnswer());
+        } else if (isTextSet) {
+            answer.setTextAnswer(answerDto.getTextAnswer());
+        }
 
         Optional<Question> question = questionRepo.findById(answerDto.getQuestionId());
-        if(question.isPresent()) {
+        if (question.isPresent()) {
             answer.setQuestion(question.get());
         } else {
             throw new QuestionNotFoundException("Question not found");
         }
 
-        Optional<User> user = userRepo.findById(answerDto.getUserId());
-        if(user.isPresent()) {
+        Optional<User> user = userRepo.findById(answerDto.getAnswererId());
+        if (user.isPresent()) {
             answer.setAnswerer(user.get());
         } else {
             throw new UserNotFoundException("User not found");
@@ -55,13 +70,12 @@ public class AnswerService extends BaseService {
         return answerRepo.save(answer);
     }
 
-
     public List<Answer> findAll() {
         return answerRepo.findAll();
     }
 
     public Optional<Answer> findOne(String id) throws AnswerNotFoundException {
-        if(answerRepo.findById(UUID.fromString(id)) != null){
+        if (answerRepo.findById(UUID.fromString(id)) != null) {
             return answerRepo.findById(UUID.fromString(id));
         }
         throw new AnswerNotFoundException();
@@ -72,7 +86,7 @@ public class AnswerService extends BaseService {
     }
 
     public List<Answer> findAllByQuestionId(UUID questionId) throws QuestionNotFoundException {
-        if(questionRepo.findById(questionId).isPresent()) {
+        if (questionRepo.findById(questionId).isPresent()) {
             return answerRepo.findByQuestionId(questionId);
         } else {
             throw new QuestionNotFoundException("Question not found");
